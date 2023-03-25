d3359dec2e4a533413906a3f0067b9b43d11140e
lewis <lewisdunning3@gmail.com>
2023-03-25 12:29:11 +0000

    Initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navItems = document.querySelectorAll('nav ul li a');
+    
+    navItems.forEach(function (navItem) {
+        navItem.addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetSection = document.querySelector(this.getAttribute('href'));
+            scrollToElement(targetSection);
+        });
+    });
+});
+
+function scrollToElement(element) {
+    var startPosition = window.pageYOffset;
+    var targetPosition = element.offsetTop - 50; // 50px offset for smooth scrolling
+    var distance = targetPosition - startPosition;
+    var duration = 1000;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+
+requestAnimationFrame(animation);
+}
